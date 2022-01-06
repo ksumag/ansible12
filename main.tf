755485690fd1f4c32a072fc9ef599f870612e366
ksumag <dymon.ksu@gmail.com>
2022-01-06 14:15:11 +0200

    complete, but login was changed over to much request for hostname to letsencrypt

--- main.tf
+++ main.tf
@@ -4,7 +4,11 @@ terraform {
       source = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
-     
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+        
     local = {
       source = "hashicorp/local"
       version = "2.1.0"
@@ -17,6 +21,11 @@ provider "local" {
 
 provider "digitalocean" {
    token = var.DO_token
+}
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.my-access-key
+  secret_key = var.my-secret-key
 }
 
 
@@ -40,12 +49,24 @@ resource "digitalocean_droplet" "ksumag" {
   ssh_keys = [digitalocean_ssh_key.my_key.fingerprint,data.digitalocean_ssh_key.rebrain_key.id ]    
 }
 
+data "aws_route53_zone" "primary" {
+  name = "devops.rebrain.srwx.net"
+  
+}
+resource "aws_route53_record" "myrecord" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "${var.login_rebrain}.${data.aws_route53_zone.primary.name}"
+  type    = "A"
+  ttl     = "300"
+  records = [digitalocean_droplet.ksumag.ipv4_address]
+}
+
 resource "local_file" "inventory_1" {
     content     = templatefile("${path.module}/inventory.tpl", { 
                            
                           access_key = var.my_private,
                           ip = digitalocean_droplet.ksumag.ipv4_address,
-                          name = digitalocean_droplet.ksumag.name
+                          name = aws_route53_record.myrecord.name 
       })
     filename = "${path.module}/inventory_1.yml"
 }
